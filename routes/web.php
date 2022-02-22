3199197f3e721a3f38092a2fa082b5800a74b2d9
tumlungaen <mr.kittinan@gmail.com>
2022-02-22 15:52:27 +0700

    try to test blade with array in js & json

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Js as Js ;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +21,9 @@ Route::get('/', function () {
         'name' => 'Kittinan Noimanee',
         'phone' => '0817846230',
         'exp' => 666,
-        'try_this' => '<br /><b>This</b> is a <u>book</u>.'
+        'try_this' => '<br /><b>This</b> is a <u>book</u>.',
+        'some_var' => 'Tum' ,
+        'my_arr' => json_encode( [ 4, 1, 8, 4, 3, 4, 5, 9 ] ) ,
     ] ) ;
 });
 
